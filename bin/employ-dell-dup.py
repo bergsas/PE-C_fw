d186e70edf876425bea1f1f1e9c79c97451ca3db
Erlend Bergsås <erlend.bergsaas@met.no>
2014-03-31 12:09:00 +0200

    Lots of changes to ''employ dell dup''

--- bin/employ-dell-dup.py
+++ bin/employ-dell-dup.py
@@ -21,6 +21,10 @@ def main():
   optparse.add_option("-1", "--one", default=False, action="store_true")
   optparse.add_option("-o", "--option", default=[], action="append")
   optparse.add_option("-k", "--keep-downloads", default=False, action="store_true")
+  optparse.add_option("-d", "--force-download", default=False, action="store_true")
+  optparse.add_option("-X", "--exclude", default=None)
+  optparse.add_option("-F", "--pattern", default=None)
+  optparse.add_option("-i", "--ignorecase", default=False, action="store_true")
   (opt, args) = optparse.parse_args()
 
   mess = {}
@@ -35,6 +39,21 @@ def main():
       xopt[x[1:]] = False
     else:
       xopt[x] = True
+  
+  if opt.ignorecase:
+    xopt['reopt'] = re.IGNORECASE
+  else:
+    xopt['reopt'] = 0
+
+  if opt.pattern:
+    opt.pattern = re.compile(opt.pattern, xopt['reopt'])
+
+  if opt.exclude:
+    opt.exclude = re.compile(opt.exclude, xopt['reopt'])
+
+  # EmployList!
+  employlist = EmployList(opt, xopt)
+ 
 
   # Read URL/file
   if not opt.url:
@@ -42,41 +61,15 @@ def main():
     cleanup(mess,opt,xopt)
     sys.exit(1)
 
-  filelist_raw = None
-  try: 
-    if bool(re.search(r'://', opt.url)):
-      if opt.verbose:
-        msg("Fetch filelist from URL: %s" %(opt.url))
-      filelist_raw = urllib2.urlopen(opt.url).read()
-    else:
-      if opt.verbose:
-        msg("Fetch filelist from local file: %s" %(opt.url))
-      filelist_raw = open(opt.url, 'r').read()
+  # Read url
+  try:
+    if not employlist.read_url(opt.url):
+      cleanup(mess, opt, xopt)
+      sys.exit(1)
   except IOError, e:
     msg(e)
-    cleanup(mess,opt,xopt)
+    cleanup(mess, opt, xopt)
     sys.exit(1)
-  
-  # If xopt has prefix, use that as prefix rather than url
-  if 'prefix' in xopt:
-    prefix = xopt['prefix']
-  else:
-    prefix = opt.url.rsplit('/', 1)[0]
-
-  # Filter filelist. :)
-  filelist = filter(lambda a: not bool(re.match("^(#.*|\s*)$", a)), 
-    filelist_raw.split("\n"))
-
-  # Create list of ... source files and target files.
-  downloads = []
-  for file in filelist:
-    filename = file.rsplit('/', 1)
-
-    if len(filename) == 1:
-      filename=filename[0]
-    else:
-      filename=filename[1]
-    downloads += [{'url': "%s/%s" % (prefix, file), 'filename': filename}]
 
   # If no opt.workdir is specified, create a tmpdir.
   tmpdir = None
@@ -84,15 +77,14 @@ def main():
     if opt.verbose:
       msg("Create temporary directory")
     
-    if not opt.silent and opt.keep_downloads:
-      msg("Warning: --keep-downloads \"crashes\" with not specifying --workdir.",
-        "Oh well. I'll go ahead and delete the stuff.")
+      if not opt.silent and opt.keep_downloads:
+        msg("Warning: --keep-downloads \"crashes\" with not specifying --workdir.",
+          "Oh well. I'll go ahead and delete the stuff.")
     tmpdir = tempfile.mkdtemp()
     if opt.verbose:
       msg("Temporary directory created: %s" %(tmpdir))
     opt.workdir = tmpdir
     mess['tmpdir'] = tmpdir
-
   else:
     if not os.path.isdir(opt.workdir):
       msg("No such directory: %s" (opt.workdir))
@@ -100,54 +92,20 @@ def main():
 
     if opt.verbose:
       msg("Use workdir: %s" %(opt.workdir))
- 
-  # Either we download all and then do magic stuff (i.e. emply 
-  #  them, or we do one and one).
-
-  no = 0
-  totnum = len(downloads)
   
-  # Do one at the time:
-  if opt.one:
-    #
-    for details in downloads:
-      if not download(details, opt, xopt, no, totnum):
-        cleanup(mess, opt, xopt)
-        sys.exit(1)
-        # Ugh. Download may have failed
-	continue
+  downloaded = employlist.fetch()
+  employlist.employ()
 
-      if not employ(details, opt, xopt, no, totnum):
-        # Ugh, employ may have failed
-	continue
 
-      if not opt.keep_downloads:
-        del_download(details, opt, xopt)
-        #  cleanup(mess, opt, xopt)
-        #	sys.exit(1)
-  else:
-    # DOwnload them all
-    for details in downloads:
-      if not download(details, opt, xopt, no, totnum):
-        cleanup(mess, opt, xopt)
-	sys.exit(1)
-        # Download failed
-	continue
-
-    # Employ them all
-    for details in downloads:
-      if not employ(details, opt, xopt, no, totnum):
-        # Emply may have failed
-	continue
-
-    # Delete them all
-    if not opt.keep_downloads:
-      for details in downloads:
-        del_download(details, opt, xopt)
-        #cleanup(mess, opt, xopt)
-        #ys.exit(1)
  
-
+#
+#    if not opt.keep_downloads:
+#      for details in downloads:
+#        del_download(details, opt, xopt)
+#        #cleanup(mess, opt, xopt)
+#        #ys.exit(1)
+# 
+#
   # Cleanup
   cleanup(mess,opt,xopt)
 
@@ -163,39 +121,140 @@ def cleanup(mess,opt,xopt):
       msg("Delete temporary directory (and content): %s" %(mess['tmpdir']))
     shutil.rmtree(mess['tmpdir'])    
 
-def del_download(details, opt, xopt):
-  rmfile = os.path.join(opt.workdir, details['filename'])
-
-  if opt.verbose:
-    msg("Delete download: %s" %(rmfile))
-  os.remove(rmfile)
-#  return os.remove(rmfile)
-
-def download(details, opt, xopt, no, totnum):
-  curl = 'curl'
-  curlopt = ['--show-error', '--remote-time', '--location']
-  if opt.silent:
-    curlopt += ["--silent"]
-  curlopt += ['--output', os.path.join(opt.workdir, details['filename']), details['url']]
+class EmployList:
+  def __init__(self, opt, xopt):
+    self.opt = opt
+    self.xopt = xopt
+    self.files = {}
   
-  if not opt.silent:
-    msg("[%d/%d]: Fetch %s from %s" %(no, totnum, details['filename'], details['url']))
+  def read_url(self, url):
+    filelist = []
+    if bool(re.search(r'://', url)):
+      if self.opt.verbose:
+        msg("Fetch filelist from URL: %s" %(url))
+      filelist = urllib2.urlopen(url).read()
+    else:
+      if self.opt.verbose:
+        msg("Fetch filelist from local file: %s" %(url))
+      filelist = open(url, 'r').read()
 
-  try:
-    exco = subprocess.call([curl] + curlopt)
-  except KeyboardInterrupt:
-    msg("KeyboardInterrupt")
-    return False
+    # If xopt has prefix, use that as prefix rather than url
+    if 'prefix' in self.xopt:
+      url_prefix = self.xopt['prefix']
+    else:
+      url_prefix = url.rsplit('/', 1)[0]
 
-  if exco != 0:
-    msg("curl failed")
-    return False
+    # Filter filelist. :)
+    filelist = filter(lambda a: not bool(re.match("^(#.*|\s*)$", a)), filelist.split("\n"))
+    
+    for file in filelist:
+      source = os.path.join(url_prefix, file)
+      target = os.path.join(self.opt.workdir, file)
+      
+      isfile = os.path.isfile(target)
 
-  return True
+      if self.opt.force_download or not isfile:
+        fetch = True
+      else: 
+        fetch = False
 
-def employ(details, opt, xopt, no, totnum):
-  print "employ"
-  return True
+      self.files[file] = {
+        'file': file,
+	'source': source,
+        'target': target,
+        'fetch': fetch,
+        'isfile': isfile,
+        'employed': False,
+        'fetch_successful': False
+      }
+
+    return True
+
+  def fetch(self, limit = None):
+    counter = 0
+    downloaded = {}
+    for file,details in self.files.items():
+      if not details['fetch']:
+        continue
+
+      counter += 1
+
+      if limit and limit < counter:
+        return downloaded
+
+      if not self.opt.silent:
+        msg("Fetch %s from %s" %(details['target'], details['source']))
+      
+      this = self.curl(details)    
+      if this:
+        downloaded[file] = this
+      else:
+        return downloaded
+    return downloaded
+
+
+  def curl(self, details):
+    curl = 'curl'
+    curlopt = ['--show-error', '--location', '--remote-time']
+    if self.opt.silent:
+      curlopt += ["--silent"]
+    curlopt += ['--output', os.path.join(self.opt.workdir, details['target']), details['source']]
+    try:
+      exco = subprocess.call([curl] + curlopt)
+    except KeyboardInterrupt:
+      msg("KeyboardInterrupt")
+      return False
+
+    if exco != 0:
+      msg("curl failed")
+      return False
+
+    details['fetch_successful'] = True
+    details['fetch'] = False
+    details['isfile'] = os.path.isfile(details['target'])
+    return details
+
+  def employ(self, downloads = None):
+    if downloads == None:
+      downloads = self.files
+
+    for file, details in downloads.items():
+      if self.opt.pattern and not self.opt.pattern.match(details['file']):
+        continue
+
+      if self.opt.exclude and self.opt.exclude.match(details['file']):
+        continue
+
+      if not details['isfile']:
+        msg("Will not employ %s: DUP is not ''detected'' as file" %(details['file']))
+        return
+      if not self.opt.silent:
+        msg("Employing: %s" % details['file'])
+      
+      sh =  ['sh', details['target'], '-n', '-q']
+      try:
+        exco = subprocess.call(sh)
+      except KeyboardInterrupt:
+        msg("KeyboardInterrupt")
+        raise KeyboardInterrupt
+      
+      details['employed'] = True
+      details['employ_exco'] = exco
+    return
+
+  def cleanup(self, files = None):
+    if self.keep_downloads:
+      return True
+     
+    if not files:
+      files = list.files
+
+    for file, details in files.items():
+      if os.path.isfile(details['target']):
+        if self.opt.verbose:
+          msg("Delete download: %s" %(details['target']))
+        os.remove(details['target'])
+  
 main()
 
 # vim: ts=2:et:ai:nocp
